8af6be8e3331248d6cbecb42c80971cebd738428
Nika AI <49638397+nika-ai@users.noreply.github.com>
2019-04-18 09:54:34 +0300

    theme names getters grouped by games

---
+++ utils/names-getters/addsubtr.js
@@ -0,0 +1,7 @@
+import {ThemeName} from 'ma-themes';
+
+export default [
+    ThemeName.RANDOM,
+    ThemeName.ADDING,
+    ThemeName.SUBTRACTION
+];
---
+++ utils/names-getters/mtable.js
@@ -0,0 +1,7 @@
+import {ThemeName} from 'ma-themes';
+
+export default [
+    ThemeName.MT_RANDOM,
+    ThemeName.MT_MULTIPLICATION,
+    ThemeName.MT_DIVISION
+];
